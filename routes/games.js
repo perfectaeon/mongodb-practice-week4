aa6e16461e392a930b336aa07fbd851d24350943
perfectaeon <andrewgwozd3221@gmail.com>
2024-04-25 22:40:31 +0600

    files

---
+++ routes/games.js
@@ -0,0 +1,9 @@
+
+const gamesRouter = require('express').Router();
+
+const findAllGames = require('../middlewares/games');
+const sendAllGames = require('../controllers/games');
+
+gamesRouter.get('/games', findAllGames, sendAllGames);
+
+module.exports = gamesRouter;
